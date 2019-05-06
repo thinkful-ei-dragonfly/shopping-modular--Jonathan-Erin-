e02596bf59a3527d9ce1bd62b7066571311b332d
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-06 14:04:54 -0500

    through section 3

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global store cuid */ //eslint-disable-line no-unused-vars
+/* global store cuid Item */ //eslint-disable-line no-unused-vars
 
 const store = (function(){ //eslint-disable-line no-unused-vars
   const items = [
@@ -10,9 +10,49 @@ const store = (function(){ //eslint-disable-line no-unused-vars
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+  const findById = function(id){ //eslint-disable-line no-unused-vars
+    store.items.find(item => item.id === id);
+  };
+  
+  const addItem = function (name){ //eslint-disable-line no-unused-vars
+    try{
+      Item.validateName(name);
+      store.items.push(Item.Create(name));
+    }
+    catch(error){
+      console.log('Cannot add item {error.message}');
+    }
+  };
+
+  const findAndToggleChecked = function(id){ //eslint-disable-line no-unused-vars
+    let item = this.findById(id);
+    item.checked = !item.checked;
+  };
+
+  const findAndUpdateName = function(id, newName){ //eslint-disable-line no-unused-vars
+    try{
+      Item.validateName(newName);
+      let item = this.findById(id);
+      item.name = item.newName;
+    }
+    catch(error){
+      console.log('Cannot update item {error.message');
+    }
+  };
+
+  const findAndDelete = function(id){ //eslint-disable-line no-unused-vars
+    const itemIndex = store.items.findIndex(item => item.id === id);
+    store.items.splice(itemIndex, 1);
+  };
+
 
   return {items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 }());
